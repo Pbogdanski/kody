4c11e43235401a1fd10874a35444c70be89b5e77
Pbogdanski <czescjannie@gmail.com>
2018-06-04 09:40:35 +0200

    pythonV2

--- Python/mlotek.py
+++ Python/mlotek.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
-
 # -*- coding: utf-8 -*-
 
 import random
 
 
 def main(args):
-    liczba = random.randint(1, 10)
-    print("wylosowano:", liczba)
-    odp = input("podaj liczbę od 1 do 10:")
-    print("podałeś:", odp)
+    liczba = random.randint(1, 10)  # losowanie liczby <1;10>
+    # print("Wylosowano:", liczba)
+    # pobranie typu użytkownika
+    for i in range(3):
+        odp = input("Podaj liczbę od 1 do 10: ")
+        print("Podałeś:", odp)
 
-    if liczba == int(odp):
-        print("zgadłeś")
-    else:
-        print("spróbuj ponownie")
+        if liczba == int(odp):  # porównanie odpowiedzi z wylosowaną liczbą
+            print("Zgadłeś!")
+            break  # przerwanie działania pętli 2xspacja # spacja - komentarz
+        else:
+            print("Spróbuj jeszcze raz!")
+
     return 0
 
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
